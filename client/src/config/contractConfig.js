f5908952e5b32c21e90cc68e668c38686cb5482c
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2025-10-11 13:50:25 +0530

    feat: configuration added

---
+++ client/src/config/contractConfig.js
@@ -0,0 +1,5 @@
+import abi from "../abi/EWA.json";
+
+export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
+export const CONTRACT_ABI = abi.abi;
+export const CHAIN_ID = import.meta.env.VITE_CHAIN_ID;
